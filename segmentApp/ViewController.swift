a8259ad132e36abf80082c2e54fa5dc2dda52b9f
ksy781 <k.s.k.s.hanahana@gmail.com>
2020-12-17 13:50:08 +0900

    Initial Commit

---
+++ segmentApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  segmentApp
+//
+//  Created by 酒井健太郎 on 2020/12/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
