e68ac28bf28a2e43db4271ad05cb497c8c5df5b0
ksy781 <k.s.k.s.hanahana@gmail.com>
2020-12-18 10:58:23 +0900

    Add Process to calculate

--- segmentApp/ViewController.swift
+++ segmentApp/ViewController.swift
@@ -14,14 +14,26 @@ final class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // The default value is noSegment (no segment selected) until the user touches a segment.
+        // Set this property to -1 to turn off the current selection.
+        segmentControll.selectedSegmentIndex = -1
     }
     
     @IBAction func segmentButtonPressed(_ sender: UISegmentedControl) {
         switch segmentControll.selectedSegmentIndex {
-        
         case 0:
             print("Tap 0")
-            label.text = "0"
+            let a = 1 + 1
+            label.text = String(a)
+        case 1:
+            print("Tap 1")
+            label.text = "1"
+        case 2:
+            print("Tap 2")
+            label.text = "2"
+        case 3:
+            print("Tap 3")
+            label.text = "3"
         default:
             print("NONE")
             label.text = "NONE"
